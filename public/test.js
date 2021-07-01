5f114b513b5f26200245a64f227111960d3a854b
Yusuf Özkan <ysfzkn58@gmail.com>
2021-07-01 16:52:20 +0300

    message

---
+++ public/test.js
@@ -0,0 +1,78 @@
+
+const searchCar = document.querySelector("#search-car");
+
+// const button = document.querySelector("#button");
+
+document.querySelector('#getads').addEventListener('click',loadads);
+
+
+function loadads(){
+
+    console.log("fonksiyon çalışıyor")
+    var loadImage = document.querySelector('#loading');
+    loadImage.style.display = 'block';
+
+    var url ='/v1/spawn/sahibinden'
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', url, true);
+   
+    setTimeout(() => 
+    {
+        xhr.onload = function()
+        {
+            loadImage.style.display="none";
+            console.log(this.responseText);
+
+            if(this.status === 200)
+            {       
+    
+                let ads = JSON.parse(this.responseText)
+                console.log(ads)
+
+                let html="";
+                
+                ads.forEach(ad => {
+                    console.log(ad.price)
+                    html+= `<tr>
+                                <td id="price">${ad.price}</td>
+                                <td id="year">${ad.year}</td>
+                                <td id="km">${ad.km}</td>
+                                <td id="color">${ad.color}</td>
+                            </tr>`;
+                });             
+    
+                document.querySelector('#ads').innerHTML = html;
+    
+            }
+        }
+        xhr.send();
+
+    }, 1500);
+}
+
+searchCar.addEventListener("keypress", (event)=>
+{
+    let text = event.target.value;
+
+    if(text !== "")
+    {
+        console.log(text);
+    }
+
+})
+
+
+// button.addEventListener('click', (updateButton)=>
+// {
+//   if (button.value === 'Start machine') 
+//   {
+//     button.value = 'Stop machine';
+//     alert("The machine has started!");
+//   } 
+//   else 
+//   {
+//     button.value = 'Start machine';
+//     alert('The machine is stopped.');
+//   }
+// })
