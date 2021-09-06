3a92e36bdce24c50f14a66e677c1a6773d94cc59
Yusuf Özkan <ysfzkn58@gmail.com>
2021-09-06 15:15:20 +0300

    v3

--- public/test.js
+++ public/test.js
@@ -1,60 +1,8 @@
 
 var loadImage = document.querySelector('#loading');
 document.querySelector('#getads').addEventListener('click',loadtable);
-//document.querySelector('#getads').addEventListener('click',loadads);
 
 let prices = []
-
-// function loadads()   // eski versiyon request işlemi
-// {
-
-//     const brand = document.getElementById("brand").value;
-//     const model = document.getElementById("model").value;   
-//     console.log(brand);
-//     console.log(model);
-    
-//     console.log("fonksiyon çalışıyor")
-
-//     loadImage.style.display = 'block';
-
-//     var url ='/v1/spawn/sahibinden'+'/'+brand+'/'+model ;
-//     console.log(url);
-//     const xhr = new XMLHttpRequest();
-
-//     xhr.open('GET', url, true);
-   
-//     setTimeout(() => 
-//     {
-//         xhr.onload = function()
-//         {
-//             loadImage.style.display="none";
-
-//             if(this.status === 200)
-//             {       
-//                 const res = JSON.parse(this.responseText)
-//                 let ads = JSON.parse(res.stdout)
-//                 console.log(ads)
-
-//                 let html="";
-                
-//                 ads.forEach(ad => {
-//                     console.log(ad.price)
-//                     html+= `<tr>
-//                                 <td id="price">${ad.price}</td>
-//                                 <td id="year">${ad.year}</td>
-//                                 <td id="km">${ad.km}</td>
-//                                 <td id="color">${ad.color}</td>
-//                             </tr>`;
-//                 });             
-    
-//                 document.querySelector('#ads').innerHTML = html;
-    
-//             }
-//         }
-//         xhr.send();
-
-//     }, 1500);
-// }
 
 function loadtable()
 {
--- src/index.js
+++ src/index.js
@@ -8,74 +8,21 @@ const util = require("util")
 const axios = require("axios")
 
 const exec = util.promisify(child.exec);
-app.use("../public",express.static(path.join(__dirname,'../public')))
+app.use("/public",express.static(path.join(__dirname,'../public')));
 
 
-// function checkSession(req, res)
-// {
-//     if(req.session.sessionId)
-//     {
-//        next();     
-//     } 
-//     else 
-//     {
-//        return res.redirect("/noauth"); 
-//     }
-// }
-
-
-
-// app.get('/auth', async (req, res) =>
-// {   
-//     const sessionId = req.query.sessionId
-
-//     if(!sessionId)
-//     {
-//     return  res.status(422).send(`<h3 align="center">Session id gerekli !</h3>`)
-//     }
-
-//     const response = await axios.get('http://api.app-platform.rgmbeta.com/api/v1/auth/getSessionInfo',
-//     {
-//         params:
-//         {
-//             sessionId:sessionId,
-//             appName:'ebrutezel/sahibinden-scrapper',
-//             secretKey:'ca139f7a53fc441189639251ac3688d9'
-//         }
-//     })
-
-//     if(!response.data.data) 
-//     {
-//         res.redirect('/noauth')
-//     }
-//     else
-//     {
-//         req.session.sessionId = sessionId;
-//         res.redirect("/");
-//     }
-// });
-
-// app.get('/noauth', function(req, res) 
-// {
-//     console.log('Yetkilendirme Başarısız !');
-//     res.status(403).send(`<h3 align="center">Yetkilendirme başarısız !</h3>`)
-// });
-
-
-app.use('/', async (req, res) => // main page
+app.get('/', async (req, res) => // main page
 {
     res.sendFile(path.join(__dirname+'/template/form.html'));
-})
+});
 
 app.get('/v1/spawn/sahibinden/:brand/:model' , async (req, res) =>
 {
-    //var parameters = req.params ; // json object
+    var parameters = req.params ; // json object
 
-    // var brand = parameters.brand;
-    // var model = parameters.model;
-    var brand = "toyota";
-    var model = "corolla";
-    console.log(brand);
+    var brand = parameters.brand;
+    var model = parameters.model;
+    
     const { stdout, stderr } = await exec(`python ${path.join(__dirname,'/script/sahibinden.py')} ${brand} ${model}`);
 
     if(stderr) 
@@ -86,10 +33,10 @@ app.get('/v1/spawn/sahibinden/:brand/:model' , async (req, res) =>
     const awat = await JSON.parse(stdout); 
     console.log(awat);
     res.send({stdout: awat})   // handling by test.js 
-})
+});
 
 
 app.listen(port, () =>
 {
     console.log(`http://localhost:${port}`)
-})
+});
